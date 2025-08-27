48dce55667714abba5280a8b60f09df58528eae0
kamruj-akash <source.akash@gmail.com>
2025-08-27 12:15:18 +0600

    count function added

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-document.getElementById("btn_id").addEventListener("click", function () {
-  alert("hu");
-});
+// re-useable function
+function getByID(id) {
+  return document.getElementById(id);
+}
+function getByClassName(id) {
+  return document.getElementsByClassName(id);
+}
+
+// call btn function
+const callCartBtn = getByClassName("card_call_btn");
+
+for (const cartBtn of callCartBtn) {
+  cartBtn.addEventListener("click", function () {
+    if (Number(getByID("coin_count").innerText) > 0) {
+      const newCoins = Number(getByID("coin_count").innerText) - 20;
+      getByID("coin_count").innerText = newCoins;
+    } else {
+      alert("Not Enough Coin");
+    }
+  });
+}
+
+// copy count function
+const copyCartBtn = getByClassName("card_copy_btn");
+
+for (const cartBtn of copyCartBtn) {
+  cartBtn.addEventListener("click", function () {
+    const newCopy = Number(getByID("copy_count").innerText) + 1;
+    getByID("copy_count").innerText = newCopy;
+  });
+}
+// heart count function
+const cartHeartBtn = getByClassName("heart_add_btn");
+
+for (const cartBtn of cartHeartBtn) {
+  cartBtn.addEventListener("click", function () {
+    const newHear = Number(getByID("heart_count").innerText) + 1;
+    getByID("heart_count").innerText = newHear;
+  });
+}
